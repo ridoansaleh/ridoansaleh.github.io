b345391b124b798280fbd4668e4a50071392f076
ridoansaleh <ridoansalehnasution98@gmail.com>
2020-08-15 06:00:23 +0700

    Updates

---
+++ precache-manifest.8df0e9ab52a0550b35967d5da472fce5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39268253ba440cb84ffd7b42c930ecdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7b0cc417dbf242be34",
+    "url": "/static/css/main.b0ccbcf6.chunk.css"
+  },
+  {
+    "revision": "4e5719c0f648242b8846",
+    "url": "/static/js/2.f990db29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f990db29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7b0cc417dbf242be34",
+    "url": "/static/js/main.07154653.chunk.js"
+  },
+  {
+    "revision": "673e5c742396be9db985",
+    "url": "/static/js/runtime-main.6ca114df.js"
+  },
+  {
+    "revision": "c77d8b83af6b041cfde46c2c7fcb779a",
+    "url": "/static/media/belajar-react-redux.c77d8b83.png"
+  },
+  {
+    "revision": "53e0be3af9e91d3afbb4128fa9fe614c",
+    "url": "/static/media/doraemon.53e0be3a.png"
+  },
+  {
+    "revision": "d4988eab29595158a8a8db761f4efa46",
+    "url": "/static/media/eat-burger.d4988eab.png"
+  },
+  {
+    "revision": "c94b8eadbc34040b8e646537d4cd0693",
+    "url": "/static/media/jasa-keuangan.c94b8ead.png"
+  },
+  {
+    "revision": "90fb784dc14e6032ed1e8225288622d6",
+    "url": "/static/media/katalog-buku.90fb784d.png"
+  },
+  {
+    "revision": "2ca49abbd9725d0faf153d973d246986",
+    "url": "/static/media/learn-gatsby-formik.2ca49abb.png"
+  },
+  {
+    "revision": "afab7fee6e2d32a18380dccf4ce6bdce",
+    "url": "/static/media/my-music.afab7fee.png"
+  },
+  {
+    "revision": "7c2bd6b85137de523ed7f1888f324349",
+    "url": "/static/media/profile.7c2bd6b8.png"
+  },
+  {
+    "revision": "86f6ebc2c062fb61ecdd378d60f5a4dc",
+    "url": "/static/media/socmed-dashboard.86f6ebc2.png"
+  },
+  {
+    "revision": "0c8cb10645f29c7c0b93996678cffd38",
+    "url": "/static/media/spotify.0c8cb106.png"
+  },
+  {
+    "revision": "ece52bcb1bccfab57a9026e4e620d5bd",
+    "url": "/static/media/vue-todo.ece52bcb.png"
+  }
+]);
