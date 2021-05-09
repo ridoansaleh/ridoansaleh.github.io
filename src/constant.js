484f65251a91e366d6f9bc9e95fb2dcaba9cd514
ridoansaleh <ridoansalehnasution98@gmail.com>
2021-05-09 21:38:44 +0700

    add new projects

--- src/constant.js
+++
@@ -1,2 +0,0 @@
-const IMAGE_BASEURL =
-  "https://raw.githubusercontent.com/ridoansaleh/ridoansaleh.github.io/master/public";
--- src/routes/projects/data.js
+++ src/routes/projects/data.js
@@ -3,6 +3,8 @@ import fbauthpagesIcon from "./assets/fb_auth_pages.png";
 import checkoutformIcon from "./assets/checkout-form.png";
 import spotifyIcon from "./assets/spotify.png";
 import katalogbukuIcon from "./assets/katalog-buku.png";
+import staffListFrontendIcon from "./assets/staff-list-frontend.png";
+import kuisSumutIcon from "./assets/kuis-sumut.png";
 import socmeddashboardIcon from "./assets/socmed-dashboard.png";
 import jasakeuanganIcon from "./assets/jasa-keuangan.png";
 import mymusicIcon from "./assets/my-music.png";
@@ -10,13 +12,21 @@ import learngatsbyIcon from "./assets/learn-gatsby-formik.png";
 import belajarreactreduxIcon from "./assets/belajar-react-redux.png";
 import vuetodoIcon from "./assets/vue-todo.png";
 import doraemonIcon from "./assets/doraemon.png";
+import staffListRestapiIcon from "./assets/staff-list-restapi.png";
+
+const PROJECT_TYPE = {
+  REACT: "react",
+  VUE: "vue",
+  NODEJS: "node.js",
+};
 
 const PROJECT_LIST = [
   {
     id: 1,
     name: "Eat Burger",
+    type: PROJECT_TYPE.REACT,
     image: eatburgerIcon,
-    altImage: "Eat Burger Screenshot",
+    alt_image: "Eat Burger Screenshot",
     repository: "https://github.com/ridoansaleh/eat-burger",
     deployment_status: "YES",
     template: "CRA",
@@ -25,8 +35,9 @@ const PROJECT_LIST = [
   {
     id: 2,
     name: "Facebook Authentication Pages",
+    type: PROJECT_TYPE.REACT,
     image: fbauthpagesIcon,
-    altImage: "Facebook Authentication Pages Screenshot",
+    alt_image: "Facebook Authentication Pages Screenshot",
     repository: "https://github.com/ridoansaleh/fb_auth_pages",
     deployment_status: "YES",
     template: "CRA",
@@ -35,8 +46,9 @@ const PROJECT_LIST = [
   {
     id: 3,
     name: "Checkout Form",
+    type: PROJECT_TYPE.REACT,
     image: checkoutformIcon,
-    altImage: "Checkout Form Screenshot",
+    alt_image: "Checkout Form Screenshot",
     repository: "https://github.com/ridoansaleh/checkout-form",
     deployment_status: "YES",
     template: "CRA TypeScript",
@@ -45,8 +57,9 @@ const PROJECT_LIST = [
   {
     id: 4,
     name: "Spotify",
+    type: PROJECT_TYPE.REACT,
     image: spotifyIcon,
-    altImage: "Spotify Screenshot",
+    alt_image: "Spotify Screenshot",
     repository: "https://github.com/ridoansaleh/spotify",
     deployment_status: "YES",
     template: "CRA",
@@ -55,8 +68,9 @@ const PROJECT_LIST = [
   {
     id: 5,
     name: "Katalog Buku",
+    type: PROJECT_TYPE.REACT,
     image: katalogbukuIcon,
-    altImage: "Katalog Buku Screenshot",
+    alt_image: "Katalog Buku Screenshot",
     repository: "https://github.com/ridoansaleh/katalog-buku",
     deployment_status: "YES",
     template: "react-csr-template",
@@ -64,78 +78,122 @@ const PROJECT_LIST = [
   },
   {
     id: 6,
+    name: "Staff List Frontend",
+    type: PROJECT_TYPE.REACT,
+    image: staffListFrontendIcon,
+    alt_image: "Staff List Frontend Screenshot",
+    repository: "https://github.com/ridoansaleh/staff_list_frontend",
+    deployment_status: "YES",
+    template: "CRA",
+    demo: "https://stupefied-boyd-354c3d.netlify.app/",
+  },
+  {
+    id: 7,
+    name: "Kuis Sumut",
+    type: PROJECT_TYPE.REACT,
+    image: kuisSumutIcon,
+    alt_image: "Kuis Sumut Screenshot",
+    repository: "https://github.com/ridoansaleh/north-sumatera-quiz",
+    deployment_status: "YES",
+    template: "CRA",
+    demo: "https://kuis-sumut.netlify.app/",
+  },
+  {
+    id: 8,
     name: "Socmed Dashboard",
+    type: PROJECT_TYPE.REACT,
     image: socmeddashboardIcon,
-    altImage: "Socmed Dashboard Screenshot",
+    alt_image: "Socmed Dashboard Screenshot",
     repository: "https://github.com/ridoansaleh/socmed_dashboard",
     deployment_status: "YES",
     template: "built from scratch",
     demo: "https://socmed-dashboard.herokuapp.com/",
   },
   {
-    id: 7,
+    id: 9,
     name: "Jasa Keuangan",
+    type: PROJECT_TYPE.REACT,
     image: jasakeuanganIcon,
-    altImage: "Jasa Keuangan Screenshot",
+    alt_image: "Jasa Keuangan Screenshot",
     repository: "https://github.com/ridoansaleh/jasa-keuangan",
     deployment_status: "YES",
     template: "CRA",
     demo: "https://ridoansaleh.github.io/jasa-keuangan/",
   },
   {
-    id: 8,
+    id: 10,
     name: "My Music",
+    type: PROJECT_TYPE.REACT,
     image: mymusicIcon,
-    altImage: "My Music Screenshot",
+    alt_image: "My Music Screenshot",
     repository: "https://github.com/ridoansaleh/MyMusic",
     deployment_status: "YES",
     template: "CRA",
     demo: "https://ridoansaleh.github.io/MyMusic/",
   },
   {
-    id: 9,
+    id: 11,
     name: "Learn Gatsby Formik",
+    type: PROJECT_TYPE.REACT,
     image: learngatsbyIcon,
-    altImage: "Learn Gatsby Formik Screenshot",
+    alt_image: "Learn Gatsby Formik Screenshot",
     repository: "https://github.com/ridoansaleh/learn-gatsby-formik",
     deployment_status: "YES",
     template: "Gatsby Default Starter",
     demo: "https://ridoansaleh.github.io/learn-gatsby-formik/",
   },
   {
-    id: 10,
+    id: 12,
     name: "Belajar React Redux",
+    type: PROJECT_TYPE.REACT,
     image: belajarreactreduxIcon,
-    altImage: "Belajar React Redux Screenshot",
+    alt_image: "Belajar React Redux Screenshot",
     repository: "https://github.com/ridoansaleh/belajar-react-redux",
     deployment_status: "YES",
     template: "CRA",
     demo: "https://ridoansaleh.github.io/belajar-react-redux/",
   },
   {
-    id: 11,
+    id: 13,
     name: "Vue Todo",
+    type: PROJECT_TYPE.VUE,
     image: vuetodoIcon,
-    altImage: "Vue Todo Screenshot",
+    alt_image: "Vue Todo Screenshot",
     repository: "https://github.com/ridoansaleh/vue-todo",
     deployment_status: "YES",
     template: "Vue CLI",
     demo: "https://ridoansaleh.github.io/vue-todo/",
   },
   {
-    id: 12,
+    id: 14,
     name: "Doraemon",
+    type: PROJECT_TYPE.REACT,
     image: doraemonIcon,
-    altImage: "Doraemon Screenshot",
+    alt_image: "Doraemon Screenshot",
     repository: "https://github.com/ridoansaleh/doraemon",
     deployment_status: "YES",
     template: "built from scratch",
     demo: "https://doraemon-rido.herokuapp.com/",
   },
+  {
+    id: 15,
+    name: "Staff List REST API",
+    type: PROJECT_TYPE.NODEJS,
+    image: staffListRestapiIcon,
+    alt_image: "Staff List REST API Screenshot",
+    repository: "https://github.com/ridoansaleh/staff_list_restapi",
+    deployment_status: "YES",
+    template: "built from scratch",
+    demo: "https://blooming-sierra-10191.herokuapp.com",
+  },
 ];
 
-export const REACT_LIST = PROJECT_LIST.filter((d) => d.id !== 11);
+const REACT_LIST = PROJECT_LIST.filter((d) => d.type === PROJECT_TYPE.REACT);
 
-export const VUE_LIST = PROJECT_LIST.filter((d) => d.id === 11);
+const VUE_LIST = PROJECT_LIST.filter((d) => d.type === PROJECT_TYPE.VUE);
+
+const NODEJS_LIST = PROJECT_LIST.filter((d) => d.type === PROJECT_TYPE.NODEJS);
+
+export { REACT_LIST, VUE_LIST, NODEJS_LIST };
 
 export default PROJECT_LIST;
