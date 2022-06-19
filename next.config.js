49a32cfa8e37890d939f47d73d0bdf2704f461f9
ridoansaleh <ridoansalehnasution98@gmail.com>
2022-06-20 06:46:08 +0700

    Fix deployment settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? '/ridoansaleh.github.io/' : '',
+  ...withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+
+  // your other plugins here
+
+  ])
+};
 
 module.exports = nextConfig
