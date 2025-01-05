14ecea30fd47dce9607bcfc1b3c79d7ea3c12ec3
Ridoan <ridoan@doerscircle.com>
2025-01-05 08:25:24 +0700

    feat: update site metadata and social links

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://ridoansaleh.github.io/",
+  title: "Portfolio",
+  name: "Ridoan Saleh",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com",
+  github: "https://github.com/ridoansaleh",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/ridoansalehnasution/",
+  email: "mailto:ridoansalehnasution98@gmail.com",
 };
